a2cdf85315391a8278f537fb8c722c52d94647cf
carloDM <carlo.de.mauro.work@gmail.com>
2023-04-14 15:53:09 +0200

    input text task new

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 /*
-
+1 sistema di aggiunta aggietto ad array tasks
 */
 
 const { createApp } = Vue ;
@@ -9,21 +9,36 @@ createApp({
     return {
       tasks: [
         {text : 'test task a', done: false, bgColor : '', priority:''},
-        {text : 'test task b', done: true, bgColor : '', priority:''},
+        {text : 'test task b', done: true,  bgColor : '', priority:''},
         {text : 'test task c', done: false, bgColor : '', priority:''},
         {text : 'test task c', done: false, bgColor : '', priority:''},
-        {text : 'test task c', done: true, bgColor : '', priority:''},
+        {text : 'test task c', done: true,  bgColor : '', priority:''},
         {text : 'test task c', done: false, bgColor : '', priority:''},
-      ]
+      ],
+
+      inputTsk : '',
+      newTask : {text : '', done: false, bgColor : '', priority:''}
 
     }
   },
 
   methods: {
+    pressEnter(){
+      this.newTask = {text : '', done: false, bgColor : '', priority:''}
+      text = this.inputTsk;
+      this.newTask.text = text;
+      this.tasks.push(this.newTask)
+      console.log('press enter',this.inputTsk,this.newTask)
+      this.inputTsk =''
+    },
 
   },
 
   mounted(){
+    addNewTask();
+    function addNewTask(){
+
+    }
     
   }
 }).mount('#app')
