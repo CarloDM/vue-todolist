16a1251c761ddba68a6b64016b35c044bc8b4f88
carloDM <carlo.de.mauro.work@gmail.com>
2023-04-14 15:26:44 +0200

    tasks obj

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
+/*
+
+*/
+
 const { createApp } = Vue ;
 
 createApp({
   data(){
     return {
+      tasks: [
+        {text : 'test task a', done: false, bgColor : '', priority:''},
+        {text : 'test task b', done: true, bgColor : '', priority:''},
+        {text : 'test task c', done: false, bgColor : '', priority:''},
+        {text : 'test task c', done: false, bgColor : '', priority:''},
+        {text : 'test task c', done: true, bgColor : '', priority:''},
+        {text : 'test task c', done: false, bgColor : '', priority:''},
+      ]
 
     }
   },
